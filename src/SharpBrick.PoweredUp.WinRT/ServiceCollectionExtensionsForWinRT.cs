5c42ce3268459b564448cea5a48278fa284965a4
T. Thiery <thomas.thiery+git@gmail.com>
2020-08-20 22:12:30 +0200

    Add ServiceCollection helper to avoid namespaces
    #79 non-breaking (new api)

---
+++ src/SharpBrick.PoweredUp.WinRT/ServiceCollectionExtensionsForWinRT.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+using SharpBrick.PoweredUp.Bluetooth;
+using SharpBrick.PoweredUp.WinRT;
+
+namespace SharpBrick.PoweredUp
+{
+    public static class ServiceCollectionExtensionsForWinRT
+    {
+        public static IServiceCollection AddWinRTBluetooth(this IServiceCollection self)
+            => self.AddSingleton<IPoweredUpBluetoothAdapter, WinRTPoweredUpBluetoothAdapter>();
+    }
+}
