1023debc14140dcf96b8c14c0f354b3b2e83f07b
T. Thiery <thomas.thiery+git@gmail.com>
2020-10-04 22:33:00 +0200

    Added Mario Static Port Information
    #91 non-breaking

--- src/SharpBrick.PoweredUp/Devices/MarioHubPants.cs
+++ src/SharpBrick.PoweredUp/Devices/MarioHubPants.cs
@@ -19,6 +19,15 @@ namespace SharpBrick.PoweredUp
 
         public IEnumerable<byte[]> GetStaticPortInfoMessages(Version softwareVersion, Version hardwareVersion)
             => @"
+0B-00-43-02-01-02-01-01-00-00-00
+05-00-43-02-02
+11-00-44-02-00-00-50-41-4E-54-00-00-00-00-00-00-00
+0E-00-44-02-00-01-00-00-00-00-00-00-7C-42
+0E-00-44-02-00-02-00-00-00-00-00-00-C8-42
+0E-00-44-02-00-03-00-00-00-00-00-00-7C-42
+0A-00-44-02-00-04-69-64-78-00
+08-00-44-02-00-05-84-00
+0A-00-44-02-00-80-01-00-03-00
 ".Trim().Split("\n").Select(s => BytesStringUtil.StringToData(s));
     }
 }
