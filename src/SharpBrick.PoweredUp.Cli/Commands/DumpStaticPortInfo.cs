0c7b07bd27674adc65cbb84c1f73d24936bde5a7
T. Thiery <thomas.thiery@gmail.com>
2020-10-05 21:02:32 +0200

    Fix initialization of protocol in CLI (#100)
    #98 non-breaking

--- src/SharpBrick.PoweredUp.Cli/Commands/DumpStaticPortInfo.cs
+++ src/SharpBrick.PoweredUp.Cli/Commands/DumpStaticPortInfo.cs
@@ -18,11 +18,11 @@ namespace SharpBrick.PoweredUp.Cli
             this.protocol = protocol ?? throw new ArgumentNullException(nameof(protocol));
             this.discoverPorts = discoverPorts ?? throw new ArgumentNullException(nameof(discoverPorts));
         }
-        public async Task ExecuteAsync(byte portId)
+        public async Task ExecuteAsync(SystemType knownSystemType, byte portId)
         {
             Console.WriteLine($"Discover Port {portId}. Receiving Messages ...");
 
-            await protocol.ConnectAsync(); // registering to bluetooth notification
+            await protocol.ConnectAsync(knownSystemType); // registering to bluetooth notification
 
             await Task.Delay(2000); // await ports to be announced initially by device.
 
