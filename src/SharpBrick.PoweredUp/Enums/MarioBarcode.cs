cd01ddab17c8a9850e8b2d0cfb51031ba44e15e4
T. Thiery <thomas.thiery+git@gmail.com>
2020-10-08 21:18:05 +0200

    Fix Goomba barcode naming
    #91 non-breaking

--- src/SharpBrick.PoweredUp/Enums/MarioBarcode.cs
+++ src/SharpBrick.PoweredUp/Enums/MarioBarcode.cs
@@ -3,7 +3,7 @@ namespace SharpBrick.PoweredUp
     public enum MarioBarcode : ushort
     {
         None = 0xFFFF,
-        Thwomp = 0x0002,
+        Goomba = 0x0002,
         Refresh = 0x0014,
         Question = 0x0029,
         Cloud = 0x002E,
