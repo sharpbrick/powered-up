27e154db87972b18439c173810e4e53c460f28d7
Rick Jansen <github@vuurbeving.nl>
2020-10-10 00:20:40 +0200

    start of bluez implementation

---
+++ src/SharpBrick.PoweredUp.BlueZ/BlueZPoweredUpBluetoothCharacteristic.cs
@@ -0,0 +1,54 @@
+﻿using HashtagChris.DotNetBlueZ;
+
+using SharpBrick.PoweredUp.Bluetooth;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Tmds.DBus;
+
+namespace SharpBrick.PoweredUp.BlueZ
+{
+    public class BlueZPoweredUpBluetoothCharacteristic : IPoweredUpBluetoothCharacteristic
+    {
+        public Guid Uuid => Guid.Empty;
+
+        private readonly GattCharacteristic _characteristic;
+
+        public BlueZPoweredUpBluetoothCharacteristic(GattCharacteristic gattCharacteristic)
+        {
+            _characteristic = gattCharacteristic ?? throw new ArgumentNullException(nameof(gattCharacteristic));
+        }
+
+        public Task<bool> NotifyValueChangeAsync(Func<byte[], Task> notificationHandler)
+        {
+            if (notificationHandler is null)
+            {
+                throw new ArgumentNullException(nameof(notificationHandler));
+            }
+
+            _characteristic.Value += ValueChangedHandler;
+
+            Task ValueChangedHandler(GattCharacteristic sender, GattCharacteristicValueEventArgs args)
+            {
+                return notificationHandler(args.Value);
+            }
+
+            return Task.FromResult(true);
+        }
+
+        public async Task<bool> WriteValueAsync(byte[] data)
+        {
+            if (data is null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            await _characteristic.WriteValueAsync(data, null);
+
+            return true;
+        }
+    }
+}
---
+++ src/SharpBrick.PoweredUp.BlueZ/BlueZPoweredUpBluetoothService.cs
@@ -0,0 +1,36 @@
+﻿using HashtagChris.DotNetBlueZ;
+using HashtagChris.DotNetBlueZ.Extensions;
+
+using SharpBrick.PoweredUp.Bluetooth;
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpBrick.PoweredUp.BlueZ
+{
+    public class BlueZPoweredUpBluetoothService : IPoweredUpBluetoothService
+    {
+        private readonly IGattService1 _service;
+
+        public Guid Uuid => Guid.Parse(_service.GetUUIDAsync().Result);
+
+
+        public BlueZPoweredUpBluetoothService(IGattService1 gattService)
+        {
+            _service = gattService ?? throw new ArgumentNullException(nameof(gattService));
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IPoweredUpBluetoothCharacteristic> GetCharacteristicAsync(Guid guid)
+        {
+            var characteristic = await _service.GetCharacteristicAsync(guid.ToString());
+
+            return new BlueZPoweredUpBluetoothCharacteristic(characteristic);
+        }
+    }
+}
