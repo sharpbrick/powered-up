ae9f5e12ee8b3b1bfd80364295278d3604dbd9f6
Rick Jansen <github@vuurbeving.nl>
2020-10-10 18:09:23 +0200

    fix getting manufacturerdata, add bt address formatter

---
+++ src/SharpBrick.PoweredUp.BlueZ/Utilities/BluetoothAddressFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace SharpBrick.PoweredUp.BlueZ.Utilities
+{
+    public class BluetoothAddressFormatter
+    {
+        public ulong ConvertToInteger(string bluetoothMacAddress) 
+            => Convert.ToUInt64(bluetoothMacAddress.Replace(":", ""), 16);
+
+        public string ConvertToMacString(ulong bluetoothAddress)
+            => string.Join(":", BitConverter.GetBytes(bluetoothAddress).Reverse().Select(b => b.ToString("X2"))).Substring(6);
+    }
+}
