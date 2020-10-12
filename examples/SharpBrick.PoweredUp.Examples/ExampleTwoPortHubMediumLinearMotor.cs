bcc2065aecf5008d5a9f2ece988189394f3c28f8
Cornelius Munz <cornelius.munz@gmx.de>
2020-10-12 07:58:42 +0200

    Add MediumLinearMotor (#105)
    #102 non-breaking (API extension, enum rename fine w/o usage)

---
+++ examples/SharpBrick.PoweredUp.Examples/ExampleTwoPortHubMediumLinearMotor.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using SharpBrick.PoweredUp;
+
+namespace Example
+{
+    public class ExampleTwoPortHubMediumLinearMotor : BaseExample
+    {
+        public override async Task ExecuteAsync()
+        {
+            using (var twoPortHub = Host.FindByType<TwoPortHub>())
+            {
+                await twoPortHub.VerifyDeploymentModelAsync(modelBuilder => modelBuilder
+                    .AddHub<TwoPortHub>(hubBuilder => hubBuilder
+                        .AddDevice<MediumLinearMotor>(twoPortHub.A)
+                    )
+                );
+
+                var motor = twoPortHub.A.GetDevice<MediumLinearMotor>();
+
+                await motor.SetAccelerationTimeAsync(3000);
+                await motor.SetDecelerationTimeAsync(1000);
+                await motor.StartSpeedForTimeAsync(6000, 90, 100, SpecialSpeed.Hold, SpeedProfiles.AccelerationProfile | SpeedProfiles.DecelerationProfile);
+
+                await Task.Delay(10_000);
+
+                await motor.StartSpeedForDegreesAsync(180, -10, 100, SpecialSpeed.Brake, SpeedProfiles.None);
+
+                await Task.Delay(10_000);
+
+                await motor.StartSpeedAsync(100, 90, SpeedProfiles.None);
+                await Task.Delay(2000);
+                await motor.StartSpeedAsync(-100, 90, SpeedProfiles.None);
+                await Task.Delay(2000);
+                await motor.StartSpeedAsync(0, 90, SpeedProfiles.None);
+
+                await Task.Delay(10_000);
+
+                await twoPortHub.SwitchOffAsync();
+            }
+        }
+    }
+}
