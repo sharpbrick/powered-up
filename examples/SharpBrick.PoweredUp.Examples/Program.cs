b88dba3a91f3e36efefe5030287a75072f98b39e
dkurok <dietmar.kurok@icloud.com>
2021-04-09 18:18:47 +0200

    Cleanup BlueGiga examples (#166)
    - nested BlueGigaBLE-project in src like the other projects
    - no need for different calls to InitHostAndDiscoverAsync()
    
    #165

--- examples/SharpBrick.PoweredUp.Examples/Program.cs
+++ examples/SharpBrick.PoweredUp.Examples/Program.cs
@@ -76,10 +76,8 @@ namespace SharpBrick.PoweredUp.Examples
             example = new Example.ExampleColorDistanceSensor();
 
             // NOTE: Examples are programmed object oriented style. Base class implements methods Configure, DiscoverAsync and ExecuteAsync to be overwriten on demand.
-            // this uses the WinRT-bluetooth-implementation by default
-            await example.InitHostAndDiscoverAsync(enableTrace);
-            //for using BlueGiga-Bluetoothadapter:
-            //await example.InitHostAndDiscoverAsync(enableTrace, bluetoothStackPort, enableTraceBlueGiga);
+            // InitHostAndDiscoverAsync uses the WinRT-bluetooth-implementation by default (bluetoothStackPort defaults to "WinRT" and enableTraceBlueGiga defaults to false)
+            await example.InitHostAndDiscoverAsync(enableTrace, bluetoothStackPort, enableTraceBlueGiga);
 
             if (example.SelectedHub is not null)
             {
