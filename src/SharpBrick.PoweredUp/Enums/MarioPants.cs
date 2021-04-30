30b6e54bddb6bc3455787191426911591f974faa
T. Thiery <thomas.thiery+git@gmail.com>
2021-04-30 21:24:08 +0200

    Updated Mario
    #174 non-breaking

--- src/SharpBrick.PoweredUp/Enums/MarioPants.cs
+++ src/SharpBrick.PoweredUp/Enums/MarioPants.cs
@@ -4,9 +4,11 @@ namespace SharpBrick.PoweredUp
     public enum MarioPants : sbyte
     {
         None = 0b00_0000,
+        Tanooki = 0b00_1010,
         Propeller = 0b00_1100,
         Cat = 0b01_0001,
         Fire = 0b01_0010,
+        Penguin = 0b01_0100,
         Normal = 0b10_0001,
         Builder = 0b10_0010,
     }
